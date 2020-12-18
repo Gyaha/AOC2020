d6b998fa36151593d79d292e2897d60c18c2a9ab
Gyaha <gyaha@users.noreply.github.com>
2020-12-18 02:52:38 +0100

    day 13 b update

--- day_13_b.py
+++ day_13_b.py
@@ -1,75 +1,57 @@
-from functools import reduce
-
-# https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
-# (This is black magic)
-
-
-def chinese_remainder(n, a):
-    sum = 0
-    prod = reduce(lambda a, b: a * b, n)
-    for n_i, a_i in zip(n, a):
-        p = prod // n_i
-        sum += a_i * mul_inv(p, n_i) * p
-    return sum % prod
+def get_busses(s: str) -> list:
+    busses = s.strip("\n").split("\n")[1].split(",")
+    busses = [(int(b), i) for i, b in enumerate(busses) if not b == "x"]
+    return busses
 
 
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
+def find_bus_alignment(s: str) -> int:
+    busses = get_busses(s)
+    i = 0
+    s = 1
+    t, la = 0, 0
+    while i < len(busses):
+        t += s
+        if does_bus_align(busses[i], t):
+            if does_bus_align(busses[i], t + t - la):
+                i += 1
+                s = t - la
+            else:
+                la = t
+    return la
 
 
-def find_next_bus_restart(s: str) -> int:
-    busses = s.strip("\n").split("\n")[1]
-    busses = [int(a) if not a == "x" else None for a in busses.split(",")]
-    b = []
-    for i, bus in enumerate(busses):
-        if not bus == None:
-            b.append([bus, -i])
-    n, a = [], []
-    for j in sorted(b):
-        n.append(j[0])
-        a.append(j[1])
-    return chinese_remainder(n, a)
+def does_bus_align(b: tuple, t: int) -> bool:
+    return (t + b[1]) % b[0] == 0
 
 
-assert chinese_remainder([27, 20], [15, 16]) == 96
-assert chinese_remainder([3, 5, 7], [2, 3, 2]) == 23
-assert chinese_remainder([7, 13, 19, 31, 59], [0, -1, -7, -6, -4]) == 1068781
-
+assert does_bus_align((7, 0), 1068781)
+assert does_bus_align((13, 1), 1068781)
+assert does_bus_align((59, 4), 1068781)
 
 test_input = """939
 7,13,x,x,59,x,31,19"""
 test_output = 1068781
-assert find_next_bus_restart(test_input) == test_output
+assert find_bus_alignment(test_input) == test_output
 
 test_input = """0
 17,x,13,19
 """
 test_output = 3417
-assert find_next_bus_restart(test_input) == test_output
+assert find_bus_alignment(test_input) == test_output
 
 test_input = """0
 67,7,59,61
 """
 test_output = 754018
-assert find_next_bus_restart(test_input) == test_output
+assert find_bus_alignment(test_input) == test_output
 
 test_input = """0
 1789,37,47,1889
 """
 test_output = 1202161486
-assert find_next_bus_restart(test_input) == test_output
+assert find_bus_alignment(test_input) == test_output
 
 f = open("inputs/input_13.txt")
 d = f.read()
 f.close()
-print(find_next_bus_restart(d))
+print(find_bus_alignment(d))
